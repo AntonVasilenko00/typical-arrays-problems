ef61277ab4f051318e9b7094db487483e956f06e
Anton <anton.vasilenko00@gmail.com>
2021-01-08 16:40:06 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  if(array === undefined || array.length == 0) return min;
+  min = array[0];
+  for(i = 1; i < array.length; i++){
+    let el = array[i];
+    if(el < min){
+      min = el;
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+  if(array === undefined || array.length == 0) return max;
+  max = array[0];
+  for(i = 1; i < array.length; i++){
+    let el = array[i];
+    if(el > max){
+      max = el;
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if(array === undefined || array.length == 0) return sum;
+  for(i = 0; i < array.length; i++){
+    sum+=array[i];
+  }
+  sum /= array.length;
+  return sum;
 }
